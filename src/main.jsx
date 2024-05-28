da6a1ceab8a6fc0be0d8f39e91f1808724b1a4e3
Andres Oliveira <andresoliveira@protonmail.com>
2024-05-28 10:04:18 -0300

    Loading between routes

--- src/main.jsx
+++ src/main.jsx
@@ -23,8 +23,10 @@ const router = createBrowserRouter(
   )
 );
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+setTimeout(() => {
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>,
+  )
+}, 6000)
